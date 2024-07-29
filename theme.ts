cbaca118ac69e6994067daba6b97f402c6ad7059
Fluturecode <fluturecode@gmail.com>
2024-07-29 18:43:25 -0400

    added theme.ts

---
+++ theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  textColor: '#000000',
+  backgroundColor: '#ffffff',
+  cardBackground: '#f0f0f0',
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: '#ffffff',
+  backgroundColor: '#1a1a1a',
+  cardBackground: '#2a2a2a',
+};
